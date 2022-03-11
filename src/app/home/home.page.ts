1e499a3a24dc29b8e5c18597481a51b8eea1f150
Moises Fernandes de Paula <you@example.com>
2022-03-11 15:09:37 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public stories: Array<any> = new Array < /any>();
+  public movies: Array<any> = new Array < /any>();
+
+  constructor(private httpClient) { }
+
+  ngOnInit() { }
+
+  ionViewDidEnter() {
+    this.loadStories();
+    this.loadMovies();
+  }
+
+  loadStories() {
+    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1'), subscribe(data => {
+      const response: any = data;
+
+      this.stories = response.Search;
+    });
+  }
+
+  loadMovies() {
+    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=love&page=1'), subscribe(data => {
+      const response: any = data;
+    });
+  }
+}
